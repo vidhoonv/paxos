2e2736749801113aa705c3497243f4b1fb7efb39
Vidhoon Viswanathan <vidhu2366@gmail.com>
2013-11-07 16:47:56 -0600

    Bug fix
    (for the continous rebroadcast issue that lead to "ran out of slots!")
    
    Signed-off-by: Vidhoon Viswanathan <vidhu2366@gmail.com>

--- replica.cpp
+++ replica.cpp
@@ -59,9 +59,11 @@ This will change along with client TENTATIVE
 				if(replica_state.decision_list.command[i] == command)	\
 				{	\
 					/*this is a repeat command which was executed before*/ \
+					printf("\nReceived REPEAT decision for command %d\n",command); \
 					repeat = true; \
 					/*skip command and update slot*/ \
 					replica_state.slot_number += 1; \
+					break;	\
 				}	\
 			}	\
 			if(!repeat)	\
@@ -70,14 +72,15 @@ This will change along with client TENTATIVE
 				replica_state.slot_number += 1; \
 				replica_state.state += 1;	\
 				respond(TALKER,client_addr[client_cmd_map[command]],client_addr_len[client_cmd_map[command]]);	\
-			}	
+			} \
+			repeat = false; \
 
 int ACCEPTOR_PORT_LIST[MAX_ACCEPTORS] = {3000,3002,3004};//,3006,3008,3010,3012,3014,3016,3018};
 int LEADER_PORT_LIST[MAX_LEADERS] = {4000,4002};
 int REPLICA_PORT_LIST[MAX_REPLICAS] = {2000,2002};
 int COMMANDER_PORT_LIST[MAX_COMMANDERS] = {5000,5001,5002,5003,5004,5005,5006,5007,5008,5009,5010,5011,5012,5013,5014,5015,5016,5017,5018,5019,5020,5021,5022,5023,5024,5025,5026,5027,5028,5029,5030,5031,5032,5033,5034,5035,5036,5037,5038,5039,5040,5041,5042,5043,5044,5045,5046,5047,5048,5049,5050,5051,5052,5053,5054,5055,5056,5057,5058,5059};
 int SCOUT_PORT_LIST[MAX_SCOUTS] = {6000,6001,6002,6003,6004,6005,6006,6007,6008,6009,6010,6011,6012,6013,6014,6015,6016,6017,6018,6019,6020,6021,6022,6023,6024,6025,6026,6027,6028,6029,6030,6031,6032,6033,6034,6035,6036,6037,6038,6039,6040,6041,6042,6043,6044,6045,6046,6047,6048,6049,6060,6051,6052,6053,6054,6055,6056,6057,6058,6059};
-int CLIENT_PORT_LIST[MAX_CLIENTS] = {7000};
+int CLIENT_PORT_LIST[MAX_CLIENTS] = {7000,7001};
 
 struct STATE_REPLICA {
 
@@ -93,6 +96,7 @@ void respond(int talker_fd,struct sockaddr dest_addr, socklen_t dest_addr_len)
 	int ret;
 	char send_buff[BUFSIZE];
 
+	printf("\nSending response to client\n"); 	
 	strcpy(send_buff,"success");
 	ret = sendto(talker_fd, send_buff, strlen(send_buff), 0, 
       		(struct sockaddr *)&dest_addr, dest_addr_len);
